1f9dcf720e0d632491e92851c8a56c8e317c617a
Csaba Nagy <n.csaba1991@gmail.com>
2023-05-09 17:51:37 +0000

    feat: add importable db file (#1)

---
+++ src/Database/exported/database.sql
@@ -0,0 +1,101 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: db
+-- Generation Time: May 09, 2023 at 05:49 PM
+-- Server version: 10.4.28-MariaDB-1:10.4.28+maria~ubu2004
+-- PHP Version: 8.1.18
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `mariadb`
+--
+CREATE DATABASE IF NOT EXISTS `mariadb` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `mariadb`;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `secrets`
+--
+
+DROP TABLE IF EXISTS `secrets`;
+CREATE TABLE `secrets` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `hash` varchar(10) NOT NULL,
+  `secret` varchar(500) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `secret_expirations`
+--
+
+DROP TABLE IF EXISTS `secret_expirations`;
+CREATE TABLE `secret_expirations` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `secret_id` int(10) UNSIGNED NOT NULL,
+  `expires_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `remaining_views` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `secrets`
+--
+ALTER TABLE `secrets`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `hash_ui` (`hash`);
+
+--
+-- Indexes for table `secret_expirations`
+--
+ALTER TABLE `secret_expirations`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `secret_id_ui` (`secret_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `secrets`
+--
+ALTER TABLE `secrets`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `secret_expirations`
+--
+ALTER TABLE `secret_expirations`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `secrets`
+--
+ALTER TABLE `secrets`
+  ADD CONSTRAINT `secret_expirations_fk` FOREIGN KEY (`id`) REFERENCES `secret_expirations` (`secret_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
