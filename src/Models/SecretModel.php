32906c30ff6505f87aa4ba108e0734dd9137623c
Csaba Nagy <n.csaba1991@gmail.com>
2023-05-09 22:06:59 +0000

    feat: add get and create methods (#3)

--- src/Models/SecretModel.php
+++ src/Models/SecretModel.php
@@ -4,23 +4,96 @@ declare(strict_types=1);
 
 namespace SecretServer\Models;
 
+use InvalidArgumentException;
+use PDOException;
 use SecretServer\Database\Database;
 use SecretServer\Models\Abstractions\BaseModel;
 
 class SecretModel extends BaseModel
 {
+  private $defaultViews = 5;
+  private $defaultExpirationInMinutes = 5;
+
   public function __construct()
   {
     parent::__construct(Database::getConnection());
   }
 
+  /**
+   *
+   * @param array $payload
+   * @return array
+   * @throws InvalidArgumentException
+   * @throws PDOException
+   */
   public function create(array $payload): array
   {
-    return [];
+    // TODO: Would be better to run it in transaction
+    $query = <<<SQL
+              insert into secrets(hash, secret)
+              values(:hash, :secret)
+              SQL;
+
+    $this->database->fetch($query, [
+      'hash' => $payload['hash'],
+      'secret' => $payload['secret']
+    ]);
+
+    $lastInsertedId = $this->database->getLastInsertedId();
+
+    $this->setExpiration($lastInsertedId);
+
+    return $this->getByHash($payload['hash']);
   }
 
+  /**
+   *
+   * @param string $hash
+   * @return null|array
+   * @throws InvalidArgumentException
+   * @throws PDOException
+   */
   public function getByHash(string $hash): ?array
   {
-    return null;
+    $query = <<<SQL
+            select
+              secrets.hash as hash,
+              secrets.secret as secretText,
+              secrets.created_at as createdAt,
+              secret_expirations.expires_at as expiresAt
+            from
+              secrets
+              join secret_expirations on 1=1
+                and secrets.id=secret_expirations.secret_id
+            where 1=1
+              and secrets.hash=:hash
+            SQL;
+
+    return $this->database->fetch($query, ['hash' => $hash]);
+  }
+
+  /**
+   *
+   * @param int $id
+   * @param null|int $minutes
+   * @param null|int $remainingViews
+   * @return void
+   * @throws InvalidArgumentException
+   * @throws PDOException
+   */
+  private function setExpiration(int $id, ?int $minutes = null, ?int $remainingViews = null): void
+  {
+    $query = <<<SQL
+              insert into secret_expirations(secret_id, expires_at, remaining_views)
+              values(:secret_id, NOW() + INTERVAL :expires_at MINUTE, :remaining_views)
+              SQL;
+
+    $expiration = $minutes ?? $this->defaultExpirationInMinutes;
+
+    $this->database->fetch($query, [
+      'secret_id' => $id,
+      'expires_at' => $expiration,
+      'remaining_views' => $remainingViews ?? $this->defaultViews
+    ]);
   }
 }
