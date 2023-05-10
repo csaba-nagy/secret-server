afa63325295a7eb4fe431498143f4f5f131b81ce
Csaba Nagy <n.csaba1991@gmail.com>
2023-05-10 15:15:06 +0000

    feat: add repository class abstraction (#5)

---
+++ src/v1/Abstracts/BaseRepository.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+namespace SecretServer\Api\v1\Abstracts;
+
+use SecretServer\Api\v1\Contracts\ModelInterface;
+
+abstract class BaseRepository
+{
+  /**
+   * @param ModelInterface $model
+   * @return void
+   */
+  public function __construct(protected ModelInterface $model)
+  {
+  }
+
+  /**
+   * @param array $payload
+   * @return array
+   */
+  abstract public function create(array $payload): array;
+}
