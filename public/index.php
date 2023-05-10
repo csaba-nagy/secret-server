b73adf956a34bffd249024abe135c71b0225dfc3
Csaba Nagy <n.csaba1991@gmail.com>
2023-05-10 18:53:18 +0000

    feat: add request class (#9)

--- public/index.php
+++ public/index.php
@@ -1,12 +1,12 @@
 <?php
 
-use SecretServer\Api\v1\Controllers\SecretController;
+use SecretServer\Api\v1\Http\Request;
 
 require __DIR__ . '/../vendor/autoload.php';
 
 try {
-  $controller = new SecretController();
-  dump($controller->get('abcde12345'));
+  $request = new Request();
+  dump($request);
 } catch (\Throwable $th) {
   dump($th->getMessage());
 }
---
+++ src/v1/Http/Request.php
@@ -0,0 +1,109 @@
+<?php
+
+declare(strict_types=1);
+
+namespace SecretServer\Api\v1\Http;
+
+use SecretServer\Enums\AllowedHttpMethods;
+
+class Request
+{
+  private string $uri;
+  private string $method;
+  private string $httpMethod;
+  private ?array $payload;
+  private array $params = [];
+  private string $acceptHeader;
+
+  public function __construct()
+  {
+    $this->uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+
+    $this->httpMethod = strtolower($_SERVER['REQUEST_METHOD']);
+
+    $this->method = match ($this->httpMethod) {
+      AllowedHttpMethods::get->value => 'get',
+      AllowedHttpMethods::post->value => 'create',
+      default => 'get'
+    };
+
+    $this->payload = $this->httpMethod === AllowedHttpMethods::post->value
+      ? json_decode(file_get_contents('php://input'), true)
+      : null;
+
+    $this->acceptHeader = $_SERVER['HTTP_ACCEPT'];
+  }
+
+  /**
+   *
+   * @return string
+   */
+  public function getUri(): string
+  {
+    return $this->uri;
+  }
+
+  /**
+   *
+   * @return string
+   */
+  public function getMethod(): string
+  {
+    return $this->method;
+  }
+
+  /**
+   *
+   * @return null|array
+   */
+  public function getPayload(): ?array
+  {
+    return $this->payload;
+  }
+
+  /**
+   *
+   * @return bool
+   */
+  public function hasPayload(): bool
+  {
+    return $this->payload !== null;
+  }
+
+  /**
+   *
+   * @return array
+   */
+  public function getExplodedUri(): array
+  {
+    return explode('/', substr($this->uri, 1));
+  }
+
+  /**
+   *
+   * @param array $params
+   * @return void
+   */
+  public function setParams(array $params): void
+  {
+    $this->params = $params;
+  }
+
+  /**
+   *
+   * @return array
+   */
+  public function getParams(): array
+  {
+    return $this->params;
+  }
+
+  /**
+   *
+   * @return string
+   */
+  public function getAcceptHeader(): string
+  {
+    return $this->acceptHeader;
+  }
+}
