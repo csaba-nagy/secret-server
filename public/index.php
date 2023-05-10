0bd0d016d7a5033cba62bbee55bc118497ca8ef3
Csaba Nagy <n.csaba1991@gmail.com>
2023-05-10 14:14:48 +0000

    revert: handle time expiration (#3)
    This reverts commit 76166148f863a65f3d3d4c81606a389f9a5f043d.

--- public/index.php
+++ public/index.php
@@ -1,24 +1,13 @@
 <?php
 
-use SecretServer\Models\SecretModel;
-use Ramsey\Uuid\Uuid;
+use SecretServer\Database\Database;
 
 require __DIR__ . '/../vendor/autoload.php';
 
 try {
-  $model = new SecretModel();
+  $db = Database::getConnection();
 
-  // Uuid::uuid4() returns with a 36 characters long string.
-  // I think for this task the 10 characters long hash is enough,
-  // and it takes up less space in the database
-  dump($model->create([
-    'hash' => substr(Uuid::uuid4()->toString(), 0, 10),
-    'secret' => 'secret text',
-    'expiresAfter' => 1,
-    'expireAfterViews' => 1
-  ]));
-
-  // dump($model->getByHash(''));
+  dump($db->isConnected());
 } catch (\Throwable $th) {
   dump($th->getMessage());
 }
