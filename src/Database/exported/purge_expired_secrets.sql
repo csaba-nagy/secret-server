b94d24cae0086d4cee52173ff2f4f05ded68923b
Csaba Nagy <n.csaba1991@gmail.com>
2023-05-11 23:09:12 +0000

    feat: add exported database event (#13)

---
+++ src/Database/exported/purge_expired_secrets.sql
@@ -0,0 +1,6 @@
+CREATE DEFINER=`mariadb`@`%` EVENT `purge_expired_secrets` ON SCHEDULE EVERY 5 MINUTE STARTS '2023-05-11 21:43:37' ON COMPLETION PRESERVE ENABLE DO DELETE secrets, secret_expirations
+FROM secrets
+	INNER JOIN secret_expirations ON 1=1
+    	AND secrets.id=secret_expirations.secret_id
+        WHERE secret_expirations.expires_at < NOW()
+        	OR secret_expirations.remaining_views = 0
