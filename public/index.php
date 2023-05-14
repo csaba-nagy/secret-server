1459c4528a402c279fd8e81b8a61e3dde29bb13d
Csaba Nagy <n.csaba1991@gmail.com>
2023-05-14 12:22:34 +0200

    chore: add dotenv package (#21)

--- public/index.php
+++ public/index.php
@@ -1,7 +1,12 @@
 <?php
 
 use SecretServer\Api\v1\Application;
+use Dotenv\Dotenv;
 
 require __DIR__ . '/../vendor/autoload.php';
+
+$dotenv = Dotenv::createImmutable(__DIR__, '../.env');
+
+$dotenv->load();
 
 Application::run();
